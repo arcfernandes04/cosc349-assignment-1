a30b4d527bdf0eae23007638caaf79a1c8ee352a
arcfernandes04 <arcfernandes04@gmail.com>
2025-10-06 19:21:23 +1300

    env updates

--- frontend/rms/src/environment.ts
+++ frontend/rms/src/environment.ts
@@ -1,1 +1,1 @@
-export const environment = { apiUrl: 'http://ec2-54-167-38-148.compute-1.amazonaws.com:3000' };
+export const environment = { apiUrl: 'http://ec2-3-82-48-221.compute-1.amazonaws.com:3000' };
